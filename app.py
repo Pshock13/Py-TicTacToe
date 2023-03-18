3dae63da0eefee908aa7f06e62396fb00ebeaed9
Pshock13 <Pshock13@live.com>
2023-03-18 16:00:32 -0400

    very basic, only one move before code ends

--- app.py
+++ app.py
@@ -1,1 +1,47 @@
-#Tic Tac Toe
+ul = "  *  "
+um = "  *  "
+ur = "  *  "
+
+ml = "  *  "
+mm = "  *  "
+mr = "  *  "
+
+bl = "  *  "
+bm = "  *  "
+br = "  *  "
+
+
+def print_board():
+    print(".................")
+    print(ul + "|" + um + "|" + ur)
+    print(".................")
+    print(ml + "|" + mm + "|" + mr)
+    print(".................")
+    print(bl + "|" + bm + "|" + br)
+    print(".................")
+
+
+print_board()
+whoseTurn = input("Who is starting? (X or O?) ").upper()
+
+move = input(whoseTurn + "'s turn. Where would you like to move? ")
+if move == "ul":
+    ul = "  " + whoseTurn + "  "
+elif move == "um":
+    um = "  " + whoseTurn + "  "
+elif move == "ur":
+    ur = "  " + whoseTurn + "  "
+elif move == "ml":
+    ml = "  " + whoseTurn + "  "
+elif move == "mm":
+    mm = "  " + whoseTurn + "  "
+elif move == "mr":
+    mr = "  " + whoseTurn + "  "
+elif move == "bl":
+    bl = "  " + whoseTurn + "  "
+elif move == "bm":
+    bm = "  " + whoseTurn + "  "
+elif move == "br":
+    br = "  " + whoseTurn + "  "
+
+print_board()
