346eaf41bc5c4afb6f92d9fbd22d327e3a3fa078
Hhavok <m.dewhurst@att.net>
2023-03-18 14:09:15 -0700

    this is my first push

--- app.py
+++ app.py
@@ -1,47 +1,35 @@
-ul = "  *  "
-um = "  *  "
-ur = "  *  "
+#global variables
+boardSpot = [" "," "," "," "," "," "," "," "," "]
+randTurn = randint(1,2)
+player1 = ""
+player2 = ""
 
-ml = "  *  "
-mm = "  *  "
-mr = "  *  "
-
-bl = "  *  "
-bm = "  *  "
-br = "  *  "
+#function logic below
+print_board()
+whoseTurn()
 
 
+#defined functions
+#whose turn prints out who goes first and assigns them to ttheir letter
+def whoseTurn(randTurn):
+    if randTurn == 1:
+        print("X goes first")
+        player1 = "X"
+        player2 = "O"
+    else:
+        print("O goes first")
+        player1 = "O"
+        player2 = "X"
+
+
+
+#this function prints the status of the board 
 def print_board():
+    print("Decide who wants to be X or O!")
     print(".................")
-    print(ul + "|" + um + "|" + ur)
+    print(boardSpot[0] + "|" + boardSpot[1] + "|" + boardSpot[2])
     print(".................")
-    print(ml + "|" + mm + "|" + mr)
+    print(boardSpot[3] + "|" + boardSpot[4] + "|" + boardSpot[5])
     print(".................")
-    print(bl + "|" + bm + "|" + br)
+    print(boardSpot[6] + "|" + boardSpot[7] + "|" + boardSpot[8])
     print(".................")
-
-
-print_board()
-whoseTurn = input("Who is starting? (X or O?) ").upper()
-
-move = input(whoseTurn + "'s turn. Where would you like to move? ")
-if move == "ul":
-    ul = "  " + whoseTurn + "  "
-elif move == "um":
-    um = "  " + whoseTurn + "  "
-elif move == "ur":
-    ur = "  " + whoseTurn + "  "
-elif move == "ml":
-    ml = "  " + whoseTurn + "  "
-elif move == "mm":
-    mm = "  " + whoseTurn + "  "
-elif move == "mr":
-    mr = "  " + whoseTurn + "  "
-elif move == "bl":
-    bl = "  " + whoseTurn + "  "
-elif move == "bm":
-    bm = "  " + whoseTurn + "  "
-elif move == "br":
-    br = "  " + whoseTurn + "  "
-
-print_board()
